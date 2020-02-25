4f15fb302738dcb22ee11ea4462b183c8ed69b1e
nsahsspecial1 <nsahsspecial1@usi.uncommonschools.org>
2020-02-25 15:19:34 -0500

    assignment one

---
+++ variables.py
@@ -0,0 +1,23 @@
+name= "dayanna"
+age=15
+
+print("my name is",name, "and i am", age)
+
+randomNum = 1.0
+futureAge= randomNum + age
+
+print("Next year i will be", futureAge)
+
+randomInt = 5
+randomFloat = 7.2
+randomBool = True
+randomNone = None
+randomString = "hi"
+
+print(type(randomInt))
+print(type(randomFloat))
+print(type(randomBool))
+print(type(randomNone))
+print(type(randomString))
+
+print("My name is " + name + " and I am " + str(age))
